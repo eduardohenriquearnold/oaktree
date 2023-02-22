be0c38f6dace4141d2e2cf3d1b6bb91d3be2e352
Eduardo Arnold <eduardoarnoldh@gmail.com>
2023-02-22 22:10:42 +0000

    Started splitting algorithm

--- src/octree.cpp
+++ src/octree.cpp
@@ -5,53 +5,94 @@
 
 #include <linalg.h>
 using namespace linalg::ostream_overloads;
-using float3 = linalg::aliases::float3;
+using double3 = linalg::aliases::double3;
 
 class OctreeNode
 {
 public:
     // Opposite extreme vertices (specify position and extent of AABB)
-    float3 vert0;
-    float3 vert1;
+    double3 vert0;
+    double3 vert1;
 
     // Point indices (for leaf nodes)
     std::vector<unsigned int> index;
 
     // Pointers to children
-    OctreeNode *child0;
-    OctreeNode *child1;
-    OctreeNode *child2;
-    OctreeNode *child3;
-    OctreeNode *child4;
-    OctreeNode *child5;
-    OctreeNode *child6;
-    OctreeNode *child7;
+    std::vector<OctreeNode *> children;
+
+    OctreeNode(double3 v0, double3 v1) : vert0(v0), vert1(v1){};
+    void erase();
 };
 
-std::vector<float3> random_pointcloud(unsigned int num_points, float spread)
+void OctreeNode::erase()
+{
+    for (OctreeNode *child : children)
+    {
+        child->erase();
+        free(child);
+    }
+};
+
+std::vector<double3> random_pointcloud(unsigned int num_points, float spread)
 {
     std::default_random_engine generator;
-    std::uniform_real_distribution<float> distribution(-spread, spread);
+    std::uniform_real_distribution<double> distribution(-spread, spread);
 
-    std::vector<float3> points;
+    std::vector<double3> points;
     for (int i = 0; i < num_points; i++)
     {
-        float3 point{distribution(generator), distribution(generator), distribution(generator)};
+        double3 point{distribution(generator), distribution(generator), distribution(generator)};
         points.push_back(point);
     }
     return points;
 }
 
-OctreeNode *create_octree(std::vector<float3> points, unsigned int max_points_per_node)
+void split_node(OctreeNode *node, unsigned int max_points_per_node, std::vector<double3> points)
 {
-    return new OctreeNode();
+    if (node->index.size() <= max_points_per_node)
+        return;
+
+    // TODO complete for-loop that creates all children with correct extents
+    double3 hsize = 0.5 * (node->vert1 - node->vert0);
+    for (int i = 0; i < 8; i++)
+    {
+        double3 delta = double3() * hsize;
+    }
+
+    double3 node_center = 0.5 * (node->vert0 + node->vert1);
+    for (unsigned int i = 0; i < node->index.size(); i++)
+    {
+        double3 point = points[node->index[i]] - node_center;
+        int children_id = (point.x > 0 ? 1 : 0) + (point.y > 0 ? 1 : 0) * 2 + (point.z > 0 ? 1 : 0) * 4;
+        node->children[children_id]->index.push_back(i);
+    }
+    node->index.clear();
+
+    // Recursively prune child without points and split child with points
+    for (auto it = node->children.begin(); it != node->children.end(); it++)
+        if ((*it)->index.size() == 0)
+        {
+            (*it)->erase();
+            node->children.erase(it--);
+        }
+        else
+            split_node(*it, max_points_per_node, points);
+}
+
+OctreeNode *create_octree(std::vector<double3> points, unsigned int max_points_per_node)
+{
+    // TODO: Get extent of points
+    OctreeNode *root = new OctreeNode(double3(-1., -1., -1.), double3(1., 1., 1.));
+
+    for (unsigned int i = 0; i < points.size(); i++)
+        root->index.push_back(i);
+
+    split_node(root, max_points_per_node, points);
+    return root;
 }
 
 int main()
 {
-    OctreeNode a;
-    std::vector<float3> pcl = random_pointcloud(100, 0.5f);
-    std::cout << a.vert0.x << std::endl;
+    std::vector<double3> pcl = random_pointcloud(100, 0.5f);
     std::cout << pcl[0] << std::endl;
-    std::cout << "Done" << std::endl;
 }
